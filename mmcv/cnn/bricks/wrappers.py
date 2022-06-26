71993c544fdff857483a08fee75ee829751faf31
jiangongwang <wangjiangong2018@ia.ac.cn>
2022-06-26 02:42:34 +0000

    mmcv for pytorch2tensorrt

--- mmcv/cnn/bricks/wrappers.py
+++ mmcv/cnn/bricks/wrappers.py
@@ -22,7 +22,8 @@ else:
 
 
 def obsolete_torch_version(torch_version, version_threshold) -> bool:
-    return torch_version == 'parrots' or torch_version <= version_threshold
+    flag = torch_version == 'parrots' or torch_version <= version_threshold
+    return flag
 
 
 class NewEmptyTensorOp(torch.autograd.Function):
@@ -40,23 +41,23 @@ class NewEmptyTensorOp(torch.autograd.Function):
 
 @CONV_LAYERS.register_module('Conv', force=True)
 class Conv2d(nn.Conv2d):
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        if x.numel() == 0 and obsolete_torch_version(TORCH_VERSION, (1, 4)):
-            out_shape = [x.shape[0], self.out_channels]
-            for i, k, p, s, d in zip(x.shape[-2:], self.kernel_size,
-                                     self.padding, self.stride, self.dilation):
-                o = (i + 2 * p - (d * (k - 1) + 1)) // s + 1
-                out_shape.append(o)
-            empty = NewEmptyTensorOp.apply(x, out_shape)
-            if self.training:
-                # produce dummy gradient to avoid DDP warning.
-                dummy = sum(x.view(-1)[0] for x in self.parameters()) * 0.0
-                return empty + dummy
-            else:
-                return empty
-
-        return super().forward(x)
+    def empty(self):
+        pass
+    # def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # if x.numel() == 0 and obsolete_torch_version(TORCH_VERSION, (1, 4)):
+            # out_shape = [x.shape[0], self.out_channels]
+            # for i, k, p, s, d in zip(x.shape[-2:], self.kernel_size,
+            #                          self.padding, self.stride, self.dilation):
+                # o = (i + 2 * p - (d * (k - 1) + 1)) // s + 1
+                # out_shape.append(o)
+            # empty = NewEmptyTensorOp.apply(x, out_shape)
+            # if self.training:
+                # # produce dummy gradient to avoid DDP warning.
+                # dummy = sum(x.view(-1)[0] for x in self.parameters()) * 0.0
+                # return empty + dummy
+            # else:
+                # return empty
+        # return super().forward(x)
 
 
 @CONV_LAYERS.register_module('Conv3d', force=True)
